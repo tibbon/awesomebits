8199f365dfef5eb839704566df4d8efa55422a98
David Fisher <tibbon@gmail.com>
2013-10-20 14:19:10 -0400

    Added two projects to the seed file to better test projects feed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,6 @@ user.update_password("gnarly")
 
 # Set up the "Any" Chapter
 Chapter.find_or_create_by_name("Any", :description => "Any Chapter", :country => COUNTRY_PRIORITY.first)
+
+Project.find_or_create_by_name("David\'s Flamethrower", url: 'http://example.org/flamethrower', email: 'david@example.com', phone: '888-555-1212', about_me: "I like flamethrowers.", chapter_id: Chapter.first.id, about_project: "I want to make a big flamethrower.", title: "Awesome flamethrower", funded_on: '2013-12-11', rss_feed_url: "https://github.com/tibbon.atom", use_for_money: "Making awesome by making flamethrowers", funded_description: "Awesome description flamethrowers")
+Project.find_or_create_by_name("Erhardt\'s Orphan", url: 'http://example.org/orphan', email: 'erhardt@example.com', phone: '888-555-2121', about_me: "I like saving orphans.", chapter_id: Chapter.first.id, about_project: "I want to save orphans.", title: "Awesome orphan saver", funded_on: '2013-10-11', rss_feed_url: "https://github.com/tibbon.atom", use_for_money: "Making awesome by saving orphans", funded_description: "Awesome description orphan")
